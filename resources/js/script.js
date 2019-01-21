b0c7e5d6daabd32ab042303226eb615ccb31326d
basiachra <basia.chra@gmail.com>
2019-01-22 00:12:07 +0100

    final touches

--- resources/js/script.js
+++ resources/js/script.js
@@ -74,6 +74,21 @@ $(document).ready(function(){
         offset: '65%'});
 
 
+    $('.js--nav-icon').click(function(){
+
+        var nav = $('.js--main-nav');
+        nav.slideToggle(200);
+
+        var icon = $('.js--nav-icon i');
+        if(icon.hasClass('ion-navicon-round')){
+            icon.addClass('ion-close-round');
+            icon.removeClass('ion-navicon-round');
+        }else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+
+    });
 
 
 
